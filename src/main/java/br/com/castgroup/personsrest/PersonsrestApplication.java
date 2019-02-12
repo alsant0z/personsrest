82d63fe52f4b94c071b43deab5905fb982677bcb
Alexandre Santos <alsant0z@gmail.com>
2019-02-12 12:23:03 -0300

    Aplicação inicial

---
+++ src/main/java/br/com/castgroup/personsrest/PersonsrestApplication.java
@@ -0,0 +1,14 @@
+package br.com.castgroup.personsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsrestApplication.class, args);
+	}
+
+}
+
